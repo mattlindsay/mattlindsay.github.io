242ed6e6de4275678c038ce76fa0c3bfdbea7254
MATT LINDSAY <mattlindsaydesign@gmail.com>
2019-05-24 10:54:09 +1000

    Update to js

--- Anaml/js/app.js
+++ Anaml/js/app.js
@@ -1,5 +1,7 @@
 // App JS
 
+// Animation Controller
+
 var anim = lottie.loadAnimation({
     container: document.getElementById('lottie'),
     renderer: 'svg',
@@ -10,3 +12,20 @@ var anim = lottie.loadAnimation({
 })
 
 anim.addEventListener('DOMLoaded',function(event){anim.playSegments([[0,89],[90,209]], true);})
+
+
+// Form Validation Controller
+
+function myFunction() {
+      // Get the checkbox
+      var checkBox = document.getElementById("formCheck");
+      // Get the output text
+      var text = document.getElementById("Errortext");
+
+      // If the checkbox is not checked, display the output text
+      if (checkBox.checked == true){
+        text.style.display = "none";
+      } else {
+        text.style.display = "block";
+      }
+    }
